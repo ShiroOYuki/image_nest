7a34658ec492ed132432fd68802e1556385504e2
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2025-02-20 18:43:35 +0800

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,21 @@ export const metadata = {
     description: 'Ciel\'s Dashboard',
     icons: {
         icon: "/imgs/favicon.ico"
+    },
+    openGraph: {
+        title: "Image Nest",
+        description: "Ciel\'s Dashboard",
+        url: "https://image-nest-preview.vercel.app",
+        siteName: "Image Nest",
+        images: [
+            {
+                url: "https://image-nest-preview.vercel.app/favicon.ico",
+                width: 256,
+                height: 256
+            }
+        ],
+        locale: "zh-tw",
+        type: "website"
     }
 };
 
